c5f6785dc12ec485b0281f566c8ebb6a8d7a725a
anderskillander <55491817+anderskillander@users.noreply.github.com>
2020-04-15 11:06:44 +0200

    Initial Commit 🚀

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// import Header from "../components/header"
+
+const IndexPage = () => {
+    return <div>
+        {/* <Header /> */}
+        <h1>Hello World</h1>
+    </div>
+}
+
+export default IndexPage
