f5f108a6239f546b70738d4687965f7971d45354
Yorshka9 <jtjeldflaat@gmail.com>
2020-04-17 14:20:03 +0200

    Feat: demo finished

--- components/Initiatives.js
+++ components/Initiatives.js
@@ -1,13 +1,21 @@
-import initiatives from "../assets/initiatives.json";
+import rawInitiatives from "../assets/initiatives.json";
 import style from "./Style/initiatives.module.scss";
 import InitiativeCard from "./InitiativeCard";
+import Filter from "./Filter";
+import { useState } from "react";
+
 
 const Initiatives = ({}) => {
+  const  [filter, setFilter] = useState ()
+  const  [initiatives, setInitiatives] = useState (rawInitiatives)
   return (
+    <div>
+      <Filter setFilter={setFilter}/>
     <div className={style.initiatives}>
       {initiatives.map((initiative, i) => {
         return <InitiativeCard data={initiative} key={i} />;
       })}
+    </div>
     </div>
   );
 };
