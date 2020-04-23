83303dc3dc0c2cc650f0fc58fac3f1a68e537b7b
augusterixon <august.erixon@hyperisland.se>
2020-04-23 12:01:54 +0200

    fix: type, sector, geographic_area is a working. Filter

--- components/Initiatives.js
+++ components/Initiatives.js
@@ -13,17 +13,50 @@ const Initiatives = ({}) => {
                setFilter(Router.router.query);
                setInitiatives(
                     rawInitiatives.filter((initiative) => {
-                         return (
-                              initiative.main_geographic_focus ===
-                                   Router.router.query.main_geographic_focus &&
-                              initiative.sector === Router.router.query.sector
-
-                              // (initiative.main_geographic_focus ===
-                              // Router.router.query.main_geographic_focus) &&
-
-                              // (initiative.main_geographic_focus ===
-                              // Router.router.query.main_geographic_focus) &&
+                         const mainArea = [
+                              initiative.policy_planning
+                                   ? 'policy_planning'
+                                   : '',
+                              initiative.knowledge_learning
+                                   ? 'knowledge_learning'
+                                   : '',
+                              initiative.finance_budgets
+                                   ? 'finance_budgets'
+                                   : '',
+                              initiative.practice_innovation
+                                   ? 'practice_innovation'
+                                   : '',
+                         ];
+                         //  console.log(Router.router.query.mainArea);
+                         console.log(
+                              Router.router.query.main_geographic_focus
+                                   ? initiative.main_geographic_focus ===
+                                          Router.router.query
+                                               .main_geographic_focus
+                                   : true && Router.router.query.sector
+                                   ? initiative.sector ===
+                                     Router.router.query.sector
+                                   : true && Router.router.query.type
+                                   ? initiative.type ===
+                                     Router.router.query.type
+                                   : true &&
+                                     //  Router.router.query.mainArea
+                                     mainArea.includes(
+                                          Router.router.query.mainArea
+                                     )
                          );
+
+                         return Router.router.query.main_geographic_focus
+                              ? initiative.main_geographic_focus ===
+                                     Router.router.query.main_geographic_focus
+                              : true && Router.router.query.sector
+                              ? initiative.sector === Router.router.query.sector
+                              : true && Router.router.query.type
+                              ? initiative.type === Router.router.query.type
+                              : true &&
+                                //  Router.router.query.mainArea
+                                mainArea.includes(Router.router.query.mainArea);
+                         // : true;
                     })
                );
           });
