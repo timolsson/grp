bf1dcbb77dbaaf3d5198128bb0107d71da10a3ec
augusterixon <august.erixon@hyperisland.se>
2020-04-24 15:13:37 +0200

    feat: added categoryColors, text overflow:scroll

--- components/Intro.js
+++ components/Intro.js
@@ -1,15 +1,26 @@
 import style from './style/intro.module.scss';
-
+import { useRef, useEffect } from 'react';
 const Intro = () => {
+     const button = useRef();
+     const intro = useRef();
+     useEffect(() => {
+          button.current.addEventListener('click', () => {
+               intro.current.style.display = 'none';
+          });
+     }, []);
      return (
           <div>
-               <input type="checkbox" className={style.button} />
+               {/* <input type="checkbox" className={style.button} /> */}
                {/* <label for="checkbox">Click me</label> */}
-               <div className={style.intro}>
+               <div ref={intro} className={style.intro}>
                     <h1>
                          Introduction to global resilience partnership
                          initiatives
                     </h1>
+                    <button ref={button} className={style.button}>
+                         {' '}
+                         x
+                    </button>
 
                     <p>
                          The Center for Climate Change and Environmental
