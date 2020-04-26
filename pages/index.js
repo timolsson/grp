29c31c8c68518d5446076c8eb667de8b2ba73143
Tim Olsson <tim.olsson@hyperisland.se>
2020-04-26 19:51:55 +0200

    intro not found fixed
    intro not found fixed

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
-
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Initiatives from "../components/Initiatives";
-import Intro from '../components/Intro';
+import Intro from "../components/Intro";
 
 const IndexPage = () => {
-     return (
-          <div>
-               <Header />
-               <Intro />
-               <Initiatives />
-          </div>
-     );
-}
+  return (
+    <div>
+      <Header />
+      <Intro />
+      <Initiatives />
+    </div>
+  );
+};
 
 export default IndexPage;
