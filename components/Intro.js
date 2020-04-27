afb2f805b432e01766a553cf9e8bd51989c8d812
augusterixon <august.erixon@hyperisland.se>
2020-04-27 11:44:41 +0200

    fix: intro, mainArea filter

--- components/Intro.js
+++ components/Intro.js
@@ -1,31 +1,74 @@
 import style from './style/intro.module.scss';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import Link from 'next/link';
 const Intro = () => {
      const button = useRef();
      const intro = useRef();
-     useEffect(() => {
-          button.current.addEventListener('click', () => {
-               intro.current.style.display = 'none';
-          });
-     }, []);
+
+     const [toggle, setToggle] = useState(true);
+
+     // useEffect(() => {
+     // button.current.addEventListener('click', () => {
+     // setToggle(!toggle);
+     //      });
+     // }, []);
+
      return (
           <div>
+               <button
+                    ref={button}
+                    className={style.button}
+                    onClick={() => {
+                         setToggle(!toggle);
+                    }}
+               >
+                    {toggle ? 'x' : 'intro'}
+               </button>
                {/* <input type="checkbox" className={style.button} /> */}
                {/* <label for="checkbox">Click me</label> */}
-               <div ref={intro} className={style.intro}>
-                    <h1>
-                         Introduction to global resilience partnership
-                         initiatives
-                    </h1>
-                    <button ref={button} className={style.button}>
-                         {' '}
-                         x
-                    </button>
+               {toggle ? (
+                    <div ref={intro} className={style.intro}>
+                         <h1>
+                              Mapping of Global and Regional Funds, Networks and
+                              Programmes
+                         </h1>
 
-                    <p>This is an intro text to what this page is about.</p>
-               </div>
+                         <p>
+                              GRP has undertaken a{' '}
+                              <a href="http://www.globalresiliencepartnership.org/resources/mapping-of-global-and-regional-funds-networks-and-programmes/">
+                                   Resilience Mapping
+                              </a>{' '}
+                              of global and regional initiatives from funds,
+                              networks, alliances, partnerships, organisations
+                              to programmes and projects that either have a
+                              focus on resilience or cover some aspect of
+                              resilience. The initiatives mapped are summarised
+                              with a short description. Their objectives are
+                              grouped under the following headings: <br />
+                              <ul>
+                                   <li>Fund and financing facilities</li>
+                                   <br />
+                                   <li>Networks, alliances and partnerships</li>
+                                   <br />
+                                   <li>Organisations</li>
+                                   <br />
+                                   <li>Programmes and projects</li>
+                              </ul>
+                              GRP welcomes the details of further initiatives
+                              and corrections to any of these summaries in this
+                              document. The should be sent to
+                              info@globalresiliencepartnership.org.
+                              <br />
+                              <br />
+                              <a href="http://www.globalresiliencepartnership.org/wp-content/uploads/2018/05/GRP-Resilience-Mapping-6-March-2020.pdf">
+                                   GRP Resilience Mapping (updated 6 March 2020
+                              </a>
+                         </p>
+                    </div>
+               ) : (
+                    ''
+               )}
           </div>
      );
 };
-
 export default Intro;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
-
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Initiatives from "../components/Initiatives";
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Initiatives from '../components/Initiatives';
 import Intro from '../components/Intro';
+import style from './style/index.module.scss';
 
 const IndexPage = () => {
      return (
           <div>
                <Header />
-               <Intro />
-               <Initiatives />
+               <div className={style.root}>
+                    <Intro />
+                    <Initiatives />
+               </div>
           </div>
      );
-}
+};
 
 export default IndexPage;
