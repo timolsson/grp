4b813aa5a1c38d02b70efa34b3b16edbe974dbaa
augusterixon <august.erixon@hyperisland.se>
2020-05-05 14:53:39 +0200

    feat: toggles for intro & data boxes, data boxes working

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,19 @@
 import style from './Style/hero.module.scss';
 import rawInitiatives from '../assets/initiatives.json';
 import HeroCard from './HeroCard';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Toggle from './Toggle';
 
 const Hero = () => {
      let arr = rawInitiatives;
-     const numberOfInitiatives = (area, x, y) => {
+     const numberOfInitiatives = (area) => {
           let results = arr.filter((x) => x.main_geographic_focus === area);
           return results.length;
      };
+
+     const intro = useRef();
+     const [toggle, setToggle] = useState(true);
+
      numberOfInitiatives('Regional - Asia');
      const cardData = [
           {
@@ -26,11 +31,10 @@ const Hero = () => {
           },
           {
                title: 'Asia',
-               stat: numberOfInitiatives(
-                    'Regional - Asia',
-                    'Regional - S. Asia',
-                    'Regional - SE Asia'
-               ),
+               stat:
+                    numberOfInitiatives('Regional - Asia') +
+                    numberOfInitiatives('Regional - S. Asia') +
+                    numberOfInitiatives('Regional - SE Asia'),
           },
           {
                title: 'Latin America',
@@ -38,25 +42,41 @@ const Hero = () => {
           },
           {
                title: 'Other',
-               stat: numberOfInitiatives('Regional - SIDS', 'Other'),
+               stat:
+                    numberOfInitiatives('Regional - SIDS') +
+                    numberOfInitiatives('Other') +
+                    numberOfInitiatives('Other (e.g. Europe NA)') +
+                    numberOfInitiatives('Regional - LDC'),
           },
      ];
 
      //   result.innerText = obj.hasOwnProperty('name');
 
+     const handleClick = () => {
+          setToggle(!toggle);
+     };
+
      return (
           <div>
-               <div className={style.hero}>
-                    {cardData.map((data, i) => {
-                         return (
-                              <HeroCard
-                                   key={i}
-                                   title={data.title}
-                                   stat={data.stat}
-                              />
-                         );
-                    })}
-               </div>
+               <Toggle
+                    click={handleClick}
+                    toggleTitle="Geographic Distribution"
+               />
+               {toggle ? (
+                    <div className={style.hero}>
+                         {cardData.map((data, i) => {
+                              return (
+                                   <HeroCard
+                                        key={i}
+                                        title={data.title}
+                                        stat={data.stat}
+                                   />
+                              );
+                         })}
+                    </div>
+               ) : (
+                    ''
+               )}
           </div>
      );
 };
--- components/Intro.js
+++ components/Intro.js
@@ -1,15 +1,14 @@
-
 import style from './Style/intro.module.scss';
 import { useRef, useEffect, useState } from 'react';
 import Link from 'next/link';
 import Aos from 'aos';
+import Toggle from './Toggle';
 
 const Intro = () => {
-  const button = useRef();
-  const intro = useRef();
+     const button = useRef();
+     const intro = useRef();
 
-  const [toggle, setToggle] = useState(true);
-
+     const [toggle, setToggle] = useState(true);
 
      // useEffect(() => {
      // button.current.addEventListener('click', () => {
@@ -20,97 +19,75 @@ const Intro = () => {
           Aos.init({ duration: 500 });
      }, []);
 
-  // useEffect(() => {
-  // button.current.addEventListener('click', () => {
-  // setToggle(!toggle);
-  //      });
-  // }, []);
-
+     // useEffect(() => {
+     // button.current.addEventListener('click', () => {
+     // setToggle(!toggle);
+     //      });
+     // }, []);
+     const handleClick = () => {
+          setToggle(!toggle);
+     };
 
      return (
           <div>
-               <button
-                    ref={button}
-                    className={style.showButton}
-                    style={!toggle ? { display: 'block' } : { display: 'none' }}
-                    // style={{
-                    //      marginRight: toggle ? '' : '6px',
-                    //      marginTop: toggle ? '' : '1px',
-                    // }}
-                    onClick={() => {
-                         setToggle(!toggle);
-                    }}
-               >
-                    {toggle ? 'x' : 'Info'}
-               </button>
-               {/* <input type="checkbox" className={style.button} /> */}
-               {/* <label for="checkbox">Click me</label> */}
+               <Toggle click={handleClick} toggleTitle="Intro" />
+
                {toggle ? (
                     <div
                          ref={intro}
                          className={style.intro}
                          data-aos="slide-up"
                     >
-                         <button
-                              ref={button}
-                              className={style.button}
-                              style={{
-                                   marginRight: toggle ? '' : '6px',
-                                   marginTop: toggle ? '' : '1px',
-                              }}
-                              onClick={() => {
-                                   setToggle(!toggle);
-                              }}
-                         >
-                              {toggle ? 'x' : 'Info'}
-                         </button>
                          <h1>
                               Mapping of Global and Regional Funds, Networks and
                               Programmes
                          </h1>
 
-          <p>
-            GRP has undertaken a{" "}
-            <a href="http://www.globalresiliencepartnership.org/resources/mapping-of-global-and-regional-funds-networks-and-programmes/">
-              Resilience Mapping
-            </a>{" "}
-            of global and regional initiatives from funds, networks, alliances,
-            partnerships, organisations to programmes and projects that either
-            have a focus on resilience or cover some aspect of resilience. The
-            initiatives mapped are summarised with a short description. Their
-            objectives are grouped under the following headings:
-          </p>
+                         <p>
+                              GRP has undertaken a{' '}
+                              <a href="http://www.globalresiliencepartnership.org/resources/mapping-of-global-and-regional-funds-networks-and-programmes/">
+                                   Resilience Mapping
+                              </a>{' '}
+                              of global and regional initiatives from funds,
+                              networks, alliances, partnerships, organisations
+                              to programmes and projects that either have a
+                              focus on resilience or cover some aspect of
+                              resilience. The initiatives mapped are summarised
+                              with a short description. Their objectives are
+                              grouped under the following headings:
+                         </p>
 
-          <ul>
-            <li>
-              Fund and financing facilities
-              <br />
-            </li>
-            <li>
-              Networks, alliances and partnerships
-              <br />
-            </li>
-            <li>
-              Organisations
-              <br />
-            </li>
-            <li>Programmes and projects</li>
-          </ul>
-          <p>
-            GRP welcomes the details of further initiatives and corrections to
-            any of these summaries in this document. The should be sent to
-            info@globalresiliencepartnership.org.
-            <br />
-            <br />
-            <a href="http://www.globalresiliencepartnership.org/wp-content/uploads/2018/05/GRP-Resilience-Mapping-6-March-2020.pdf">
-              GRP Resilience Mapping (updated 6 March 2020)
-            </a>
-          </p>
-        </div>
-      ) : (
-        ""
-      )}
-    </div>
-  );
+                         <ul>
+                              <li>
+                                   Fund and financing facilities
+                                   <br />
+                              </li>
+                              <li>
+                                   Networks, alliances and partnerships
+                                   <br />
+                              </li>
+                              <li>
+                                   Organisations
+                                   <br />
+                              </li>
+                              <li>Programmes and projects</li>
+                         </ul>
+                         <p>
+                              GRP welcomes the details of further initiatives
+                              and corrections to any of these summaries in this
+                              document. The should be sent to
+                              info@globalresiliencepartnership.org.
+                              <br />
+                              <br />
+                              <a href="http://www.globalresiliencepartnership.org/wp-content/uploads/2018/05/GRP-Resilience-Mapping-6-March-2020.pdf">
+                                   GRP Resilience Mapping (updated 6 March 2020)
+                              </a>
+                         </p>
+                    </div>
+               ) : (
+                    ''
+               )}
+          </div>
+     );
 };
 export default Intro;
