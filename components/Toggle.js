861fbe60b8967794691c3d1e66167892b7dead9a
Tim Olsson <tim.olsson@hyperisland.se>
2020-05-07 09:53:39 +0200

    fixed typo
    Now gave error on typo for Style

--- components/Toggle.js
+++ components/Toggle.js
@@ -1,31 +1,31 @@
-import { useState, useRef, useEffect } from 'react';
-import style from './style/toggle.module.scss';
+import { useState, useRef, useEffect } from "react";
+import style from "./Style/toggle.module.scss";
 
 const Toggle = (props) => {
-     //  const button = useRef();
-     //  const [toggle, setToggle] = useState(true);
-     const input = useRef();
+  //  const button = useRef();
+  //  const [toggle, setToggle] = useState(true);
+  const input = useRef();
 
-     useEffect(() => {
-          input.current.checked = props.isToggled;
-          console.log(props.isToggled);
-     }, []);
+  useEffect(() => {
+    input.current.checked = props.isToggled;
+    console.log(props.isToggled);
+  }, []);
 
-     return (
-          <div className={style.container}>
-               <label class={style.switch}>
-                    <input
-                         ref={input}
-                         type="checkbox"
-                         onClick={(e) => {
-                              props.click();
-                         }}
-                    />
-                    <span className={style.slider} />
-               </label>
-               <h5>{props.toggleTitle}</h5>
-          </div>
-     );
+  return (
+    <div className={style.container}>
+      <label class={style.switch}>
+        <input
+          ref={input}
+          type="checkbox"
+          onClick={(e) => {
+            props.click();
+          }}
+        />
+        <span className={style.slider} />
+      </label>
+      <h5>{props.toggleTitle}</h5>
+    </div>
+  );
 };
 
 export default Toggle;
--- components/Toggles.js
+++ components/Toggles.js
@@ -1,23 +1,23 @@
-import style from './style/toggles.module.scss';
-import Toggle from './Toggle';
-import { useState, useEffect } from 'react';
+import style from "./Style/toggles.module.scss";
+import Toggle from "./Toggle";
+import { useState, useEffect } from "react";
 
 const Toggles = ({ toggle }) => {
-     return (
-          <div className={style.toggles}>
-               {toggle
-                    ? toggle.map((t) => {
-                           return (
-                                <Toggle
-                                     click={t.click}
-                                     toggleTitle={t.name}
-                                     isToggled={t.isToggled}
-                                />
-                           );
-                      })
-                    : ''}
-          </div>
-     );
+  return (
+    <div className={style.toggles}>
+      {toggle
+        ? toggle.map((t) => {
+            return (
+              <Toggle
+                click={t.click}
+                toggleTitle={t.name}
+                isToggled={t.isToggled}
+              />
+            );
+          })
+        : ""}
+    </div>
+  );
 };
 
 export default Toggles;
